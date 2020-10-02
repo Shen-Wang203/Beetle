56180d7708d235acba1be8b6c38b2b7b25e29f2d
yushenhu <yushenhu@ucla.edu>
2020-10-02 10:17:25 -0700

    backup 1002 Wesley

--- main_codes/HPP_Control.py
+++ main_codes/HPP_Control.py
@@ -163,11 +163,11 @@ Tser2 = serial.Serial('COM13', 115200, timeout=None, stopbits=1)
 
 # Control box #6
 # COM4: T3
-# Tser3 = serial.Serial('COM4', 115200, timeout=0.1, stopbits=1)
+#Tser3 = serial.Serial('COM4', 115200, timeout=0.1, stopbits=1)
 # COM6: T1
-# Tser1 = serial.Serial('COM6', 115200, timeout=0.1, stopbits=1)
+#Tser1 = serial.Serial('COM6', 115200, timeout=0.1, stopbits=1)
 # COM5: T2
-# Tser2 = serial.Serial('COM5', 115200, timeout=0.1, stopbits=1)
+#Tser2 = serial.Serial('COM5', 115200, timeout=0.1, stopbits=1)
 
 
 error_log = ''
